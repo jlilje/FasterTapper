2e65646e127f9335f6d32d7500ea119891579293
Jeremy Lilje <jeremylilje@Jeremys-MacBook-Pro.local>
2016-05-23 14:52:55 -0400

    initial commit of xcode project

---
+++ FasterTapper/FasterTapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FasterTapper
+//
+//  Created by Jeremy Lilje on 5/23/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
